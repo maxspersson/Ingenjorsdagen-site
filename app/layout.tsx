498800b31431ad837ef9d0a4e3e44edc42dfd8a8
Max Persson <maxpersson@Max-Macbook-Air.lan>
2026-07-03 09:26:07 +0200

    GTm Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,22 +32,30 @@ export default function RootLayout({
   return (
     <html lang="sv">
       <head>
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-0HSK3KT0V2"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0HSK3KT0V2');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N7T9WM4W');
           `}
         </Script>
       </head>
 
       <body className={`${lora.variable} ${lora.className}`}>
+        {/* Google Tag Manager noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N7T9WM4W"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <SiteHeader />
 
         <main className="pt-[126px] sm:pt-[140px] md:pt-[186px]">
