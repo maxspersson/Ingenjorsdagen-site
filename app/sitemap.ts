1a4aae92c06931a7f889eaa48502538d6f8fa5e7
Max Persson <maxpersson@Max-Macbook-Air.lan>
2026-07-03 10:10:41 +0200

    Sitemap-v2

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,21 +3,27 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://ingenjorsdagen.se";
 
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/heroes`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/grand-prize`,
-      lastModified: new Date(),
-    },
-    // osv...
-  ];
+  const routes = [
+  "",
+  "/engineering-day-2026",
+  "/attending-heroes",
+  "/the-grand-prize-for-engineering",
+  "/the-grand-prize-for-engineering/innovation",
+  "/the-grand-prize-for-engineering/leadership",
+  "/the-grand-prize-for-engineering/rising-star",
+  "/the-grand-prize-for-engineering/role-model-of-the-year",
+  "/the-grand-prize-for-engineering/sustainability",
+  "/pre-events",
+  "/pre-events/goteborg",
+  "/pre-events/malmo",
+  "/pre-events/stockholm",
+  "/contact-us",
+];
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: route === "" ? 1 : 0.8,
+  }));
 }
